0fcb75d3b499ff9e11eefd59ed8df67901922361
Yichen Song <yichen@indeed.com>
2018-11-22 02:15:59 -0600

    scrape yahoo revenue report #3

---
+++ djinn/crawler/yahoo_income_statement.py
@@ -0,0 +1,32 @@
+import requests
+import locale
+from bs4 import BeautifulSoup
+from crawler import YAHOO_FINANCIAL_REPORT_URL
+
+
+def get_income_statement(stock):
+    html = requests.get(YAHOO_FINANCIAL_REPORT_URL.format(stock.lower())).text
+    soup = BeautifulSoup(html, "lxml")
+
+    matched_doms = soup.select("#mrt-node-Col1-3-Financials table tbody")
+
+    if matched_doms is None:
+        return {}
+    else:
+        rows = matched_doms[0].find_all("tr")
+        result = {}
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+        for i in range(1, 4):
+            row = rows[i]
+            name = __get_cell_value(row, 0)
+            value = locale.atoi(__get_cell_value(row, 1))
+
+            result[name] = value
+
+        return result
+
+
+def __get_cell_value(row, col):
+    return row.contents[col].find("span").text if row.contents[col].find("span") is not None else row.contents[col].text
+
